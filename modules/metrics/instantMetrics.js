95362b7e33bbfcb9fbb9d7b833da5fdfbda60bba
Taras Kontsur <kontsur@gmail.com>
2020-05-18 23:36:30 +0300

    Improved metrics

--- modules/metrics/instantMetrics.js
+++ modules/metrics/instantMetrics.js
@@ -6,7 +6,7 @@ class InstantMetrics {
     const t = days[0];
     const y = days[1];
     const currentTrend = ratesHistory.getLatestRateSync(type, t.currency);
-    
+
     /*logger.info(`T: ${t.date} ${t.ask} ${t.bid} ${t.trendAsk} ${t.trendBid}`);
     logger.info(`Y: ${y.date} ${y.ask} ${y.bid} ${y.trendAsk} ${y.trendBid} ${y.maxAsk} ${y.minBid}`);
     logger.info(`Trend: ${currentTrend.date} ${currentTrend.maxAsk} ${currentTrend.minBid} ${currentTrend.trend}`);*/
@@ -33,7 +33,7 @@ class InstantMetrics {
       if (t.ask < y.minBid && t.trendAsk < 0) {
         return -1;
       }
-      
+
       // if data is controversional then check against the day before
       const y1 = days.find(
         (d) => d.trendAsk * t.trendAsk < 0 || d.trendBid * t.trendBid < 0
@@ -41,16 +41,30 @@ class InstantMetrics {
       if (!y1 || !y1.date.isBefore(y.date, 'd')) {
         return 0;
       }
-  
+
       if (t.ask < y1.minBid && t.trendAsk < 0) {
         return -2;
       }
-  
+
       if (t.bid > y1.maxAsk && t.trendBid > 0) {
         return 2;
       }
     }
 
+    // trends are misleading
+    if (t.trendAsk * (t.ask - y.ask) < 0 || t.trendBid * (t.bid - y.bid) < 0) {
+      const realAsk = t.ask - y.ask;
+      const realBid = t.bid - y.bid;
+      // minimum is more than 1% higher than maximum yesterday
+      if (t.bid > y.maxAsk && realBid > 0) {
+        return 3;
+      }
+
+      // maximum is more than 1% lower than minimum yesterday
+      if (t.ask < y.minBid && realAsk < 0) {
+        return -3;
+      }
+    }
     return 0;
   }
 }
